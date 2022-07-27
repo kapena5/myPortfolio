826f3ff3f89567bbf991a2e16bfb3a96ac011fd9
Che <che.schumacher@gmail.com>
2022-07-27 17:31:27 -0600

    added second animation

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 $('.projectsLink').click(function() {
     $('html, body').animate({
-        scrollTop: $(".projectsHeader").offset().top
+        scrollTop: $('.projectsHeader').offset().top - 50
       });
+      
+      $('.projectsHeader').animate({
+        fontSize: "60px"
+      }, "slow");
+      $('.projectsHeader').animate({
+        fontSize: "50px"
+      }, "slow");
 });
