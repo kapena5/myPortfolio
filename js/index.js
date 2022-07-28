23ffc9f92fc4453f04d0ef9ad16331c5af7cdaf9
Che <che.schumacher@gmail.com>
2022-07-27 18:56:37 -0600

    Updated responsive hero section

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ $('.projectsLink').click(function() {
     $('html, body').animate({
         scrollTop: $('.projectsHeader').offset().top - 50
       });
-      
       $('.projectsHeader').animate({
         fontSize: "60px"
       }, "slow");
