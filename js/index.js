840dbbdf2beb1facb424b4f1f0ba7c2a0aaedd50
Che <che.schumacher@gmail.com>
2022-08-04 14:09:14 -0600

    Fixed nav

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
+$('.aboutLink').click(function() {
+  $('html, body').animate({
+      scrollTop: $('.aboutHeader').offset().top - 50
+    });
+    $('.aboutHeader').animate({
+      fontSize: "60px"
+    }, "slow");
+    $('.aboutHeader').animate({
+      fontSize: "50px"
+    }, "slow");
+});
+$('.skillsLink').click(function() {
+  $('html, body').animate({
+      scrollTop: $('.skillsHeader').offset().top - 50
+    });
+    $('.skillsHeader').animate({
+      fontSize: "60px"
+    }, "slow");
+    $('.skillsHeader').animate({
+      fontSize: "50px"
+    }, "slow");
+});
 $('.projectsLink').click(function() {
     $('html, body').animate({
         scrollTop: $('.projectsHeader').offset().top - 50
@@ -8,4 +30,15 @@ $('.projectsLink').click(function() {
       $('.projectsHeader').animate({
         fontSize: "50px"
       }, "slow");
+});
+$('.contactLink').click(function() {
+  $('html, body').animate({
+      scrollTop: $('.contactHeader').offset().top - 50
+    });
+    $('.contactHeader').animate({
+      fontSize: "60px"
+    }, "slow");
+    $('.contactHeader').animate({
+      fontSize: "50px"
+    }, "slow");
 });
