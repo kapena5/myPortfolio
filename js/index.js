cd4a82fae1c00032b326459a5376fcab639a0e3a
Che <che.schumacher@gmail.com>
2022-09-07 08:50:14 -0600

    added contact form

--- js/index.js
+++ js/index.js
@@ -42,26 +42,29 @@ gsap.utils.toArray(".revealUp").forEach(function (elem) {
   });
 });  
 
+const scrollTime = 1000;
+const offSet = 50;
+
 //scroll to header after button click
 $('.aboutLink').click(function() {
   $('html, body').animate({
-      scrollTop: $('.aboutHeader').offset().top - 50
-    }, 1300);
+      scrollTop: $('.aboutHeader').offset().top - offSet
+    }, scrollTime);
 });
 $('.skillsLink').click(function() {
   $('html, body').animate({
-      scrollTop: $('.skillsHeader').offset().top - 50
-    }, 1300);
+      scrollTop: $('.skillsHeader').offset().top - offSet
+    }, scrollTime);
 });
 $('.projectsLink').click(function() {
     $('html, body').animate({
-        scrollTop: $('.projectsHeader').offset().top - 50
-      }, 1300);
+        scrollTop: $('.projectsHeader').offset().top - offSet
+      }, scrollTime);
 });
 $('.contactLink').click(function() {
   $('html, body').animate({
-      scrollTop: $('.contactHeader').offset().top - 50
-    }, 1300);
+      scrollTop: $('.contactHeader').offset().top - offSet
+    }, scrollTime);
 });
 
 //deactivate scroll if hambuger menu is expanded 
